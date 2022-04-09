c0a95ccfe967b61eb530f5af24039e46c2b92619
anwersayeed <deeyasrewna@gmail.com>
2022-04-09 14:21:49 +0530

    Created OTP Login Page

---
+++ my-app/src/App.js
@@ -0,0 +1,14 @@
+import OTP from "./components/OTP";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <OTP />
+      </header>
+    </div>
+  );
+}
+
+export default App;
